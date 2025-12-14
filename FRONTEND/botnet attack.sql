849a9b76a6ef78ef519f9ad14043d1fd1b8752c2
Prathibha7 <prathibha732004@gmail.com>
2025-12-14 15:13:09 +0530

    Update SQL file

--- FRONTEND/botnet attack.sql
+++ FRONTEND/botnet attack.sql
@@ -1,18 +1,16 @@
--- Drop the table if it exists
-DROP TABLE IF EXISTS `users`;
 
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS botnetattack;
-
--- Switch to the created database
-USE botnetattack;
-
--- Create the user table
-CREATE TABLE `users` (
-    `name` VARCHAR(225),
-    `email` VARCHAR(225),
-    `password` VARCHAR(225),
-    `Address` VARCHAR(225)
+CREATE TABLE IF NOT EXISTS `users` (
+  `name` VARCHAR(225),
+  `email` VARCHAR(225),
+  `password` VARCHAR(225),
+  `Address` VARCHAR(225)
 );
+
+
+
+
+
+
+
 
 
